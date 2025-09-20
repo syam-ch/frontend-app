e9430de68527e140882ea576ff933be5d858490a
syam-ch <chundurusyam26@gmail.com>
2025-09-20 11:40:51 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>🚀 E-Commerce Sample</h1>
+      <p>Welcome! Products will load from backend APIs.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // optional, if you want global styles
+
+// Get the root div from public/index.html
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component inside the root div
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
